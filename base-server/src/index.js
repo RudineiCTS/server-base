fadfe6816ce2713f5fce097b634c956f3d23fa5d
Rudinei Costa <costaaugusto177@outlook.com>
2020-08-23 15:06:29 -0300

    tipos de rotas, e metodos https

--- base-server/src/index.js
+++ base-server/src/index.js
@@ -4,7 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.json());
+
 app.get('/project', (request, response)=>{
+    const query = request.query;
+
     return response.json([
         'projeto1',
         'projeto2'
@@ -12,8 +16,27 @@ app.get('/project', (request, response)=>{
     ])
 });
 
-app.post('/project', (request, response)=>{
+app.post('/projects', (request, response)=>{
+    const body = request.body;
+
     return response.json([
+        'projeto1',
+        'projeto2',
+        'projeto3',
+    ])
+});
+app.put('/projects/:id', (request, response)=>{
+    const params = request.params;
+
+
+    return response.json([
+        'projeto1',
+        'projeto2',
+        'projeto3',
+    ])
+});
+app.delete('/projects/:id', (request, reponse)=>{
+    return reponse.json([
         'projeto1',
         'projeto2',
         'projeto3',
